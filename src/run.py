d95460843f77bc5ca2e85f112dc8e603f7d0c377
Janan Ahmadi <JananAhmadiOZ@gmail.com>
2026-06-09 17:24:37 +0330

    updating st project

--- src/run.py
+++ src/run.py
@@ -1,7 +1,20 @@
 import streamlit as st
+import json
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+try:
+    from stringIO import StringIO
+except ImportError:
+    from io import StringIO
+
 st.title("pytopia dashboard")
 
-print("ali hejazizo")
+with st.expander('Statistics'):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    sns.histplot(np.random.randn(100), ax=ax)
+    st.pyplot(fig)
 
 
-print("hamid mahdavi")
+
